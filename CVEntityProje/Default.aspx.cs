622fb79a1671f9ddca550aef9073a4bb7d0d8ad5
Büşra Kınacı <75859913+busrakinacii@users.noreply.github.com>
2024-10-03 21:37:43 +0300

    Add project files.

---
+++ CVEntityProje/Default.aspx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CVEntityProje
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        DbCvEntityEntities db = new DbCvEntityEntities();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Repeater1.DataSource = db.TBLHAKKIMDA.ToList();
+            Repeater1.DataBind();
+
+            Repeater2.DataSource = db.TBLHAKKIMDA.ToList();
+            Repeater2.DataBind();
+
+            Repeater3.DataSource = db.TBLHAKKIMDA.ToList();
+            Repeater3.DataBind();
+        }
+    }
+}
